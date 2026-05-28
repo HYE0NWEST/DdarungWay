00f37fb47f1772a474c98de75680143db97bd81c
Kim, Hyeonseo <hsys20020702@gmail.com>
2026-05-28 22:30:07 +0900

    fix: add Toaster component to App to enable toast notifications

--- bikepulse-frontend/src/App.tsx
+++ bikepulse-frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
 import { queryClient } from './lib/queryClient';
 import { appRouter } from './app/router';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={appRouter} />
+      <Toaster position="top-center" />
     </QueryClientProvider>
   );
 }
