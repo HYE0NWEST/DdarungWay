32f043a1f1832d7747ac236d8f9fe235bc9374cd
Kim, Hyeonseo <hsys20020702@gmail.com>
2026-05-29 07:56:36 +0900

    알림 오류 수정

--- bikepulse-frontend/src/App.tsx
+++ bikepulse-frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { Toaster } from 'react-hot-toast';
 import { queryClient } from './lib/queryClient';
 import { appRouter } from './app/router';
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={appRouter} />
-      <Toaster position="top-center" />
     </QueryClientProvider>
   );
 }
